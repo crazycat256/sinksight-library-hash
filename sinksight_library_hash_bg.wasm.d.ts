676fd1ddd611c3e86f337b7f7dfb3c3d8d07c4f7
crazycat256 <crazycat256@users.noreply.github.com>
2026-09-19 10:31:32 +0000

    Publish prebuilt WASM package 0.1.3 5fd1a22792869ad003270cdd808a09e600a83c88

---
+++ sinksight_library_hash_bg.wasm.d.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const buildDb: (a: number, b: any, c: any, d: any) => [number, number, number, number];
+export const checkScript: (a: number, b: number, c: number) => any;
+export const extractDbContents: (a: number) => any;
+export const extractHashes: (a: number, b: number, c: number) => [number, number, number];
+export const freeDb: (a: number) => void;
+export const listLibs: (a: number) => any;
+export const loadDb: (a: number, b: number) => [number, number, number];
+export const parseHashBytes: (a: number, b: number) => any;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
